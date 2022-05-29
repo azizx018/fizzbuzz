429b418cd05bf49158427bb468a86613e2ad6405
azizx018 <chiara.aziz@gmail.com>
2022-05-29 15:51:52 -0500

    2 backend modules created

---
+++ fizzbuzz-be/src/main/java/net/yorksolutions/fizzbuzzbe/FizzbuzzBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.fizzbuzzbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzBeApplication.class, args);
+	}
+
+}
---
+++ fizzbuzz-be/src/test/java/net/yorksolutions/fizzbuzzbe/FizzbuzzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.fizzbuzzbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
