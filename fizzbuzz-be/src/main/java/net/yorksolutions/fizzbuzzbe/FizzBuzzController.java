99618aaf3e1e1b96a70484fdbc053f57fdd62bef
azizx018 <chiara.aziz@gmail.com>
2022-05-29 16:27:11 -0500

    fizzbuzz and user both run

--- fizzbuzz-be/src/main/java/net/yorksolutions/fizzbuzzbe/FizzBuzzController.java
+++ fizzbuzz-be/src/main/java/net/yorksolutions/fizzbuzzbe/FizzBuzzController.java
@@ -20,24 +20,17 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/")
 public class FizzBuzzController {
-    private final UserAccountRepository repository;
-    private final HashMap<UUID, Long> tokenMap;
 
     private final RestTemplate rest;
 
     // This is for Spring
     @Autowired
-    public FizzBuzzController(@NonNull UserAccountRepository repository) {
-        this.repository = repository;
-        tokenMap = new HashMap<>();
+    public FizzBuzzController() {
         rest = new RestTemplate();
     }
 
     // This is for Mockito
-    public FizzBuzzController(@NonNull UserAccountRepository repository,
-                              @NonNull HashMap<UUID, Long> tokenMap, RestTemplate rest) {
-        this.repository = repository;
-        this.tokenMap = tokenMap;
+    public FizzBuzzController( RestTemplate rest) {
         this.rest = rest;
     }
 
@@ -68,43 +61,10 @@ public class FizzBuzzController {
         return FizzBuzz.play(input);
     }
 
-    // What we want:
-    // {
-    //     "ip": "1.2.3.4"
-    // }
 
     public IP ip(UUID token, HttpServletRequest request) {
-        if (!tokenMap.containsKey(token))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-
+        checkAuthorized(token);
         return new IP(request.getRemoteAddr());
     }
 
-    @GetMapping("/login")
-    UUID login(@RequestParam String username, @RequestParam String password) {
-        // check to see if the username exists
-        // Search for a UserAccount with the given username and password
-        var result = repository.findByUsernameAndPassword(username, password);
-
-        // If not found, inform the client that they are unauthorized
-        if (result.isEmpty())
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-
-        // If found:
-        // Generate a token that is to be used for all future requests that are associated
-        //     w/ this user account
-        final var token = UUID.randomUUID();
-        // Associate the generated token w/ the user account
-        tokenMap.put(token, result.get().id);
-        // Provide the generated token to the client for future use
-        return token; // from now on, use this uuid to let me know who you are
-    }
-
-    @GetMapping("/register")
-    public void register(@RequestParam String username, @RequestParam String password) {
-        if (repository.findByUsername(username).isPresent())
-            throw new ResponseStatusException(HttpStatus.CONFLICT);
-
-        repository.save(new UserAccount());
-    }
 }
