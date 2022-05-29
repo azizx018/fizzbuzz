40af67f6b0d61d22a461e9271fced08b3efdf1df
azizx018 <chiara.aziz@gmail.com>
2022-05-29 17:00:02 -0500

    update

--- fizzbuzz-be/src/test/java/net/yorksolutions/fizzbuzzbe/IPTest.java
+++ fizzbuzz-be/src/test/java/net/yorksolutions/fizzbuzzbe/IPTest.java
@@ -21,24 +21,8 @@ class IPTest {
     void itShouldJsonify() throws JsonProcessingException {
         final String ip = "1.2.3.4";
         assertEquals(
-                "{\"ip\":\""+ip+"\"}",
+                "{\"ip\":\"" + ip + "\"}",
                 new ObjectMapper().writeValueAsString(new IP(ip))
         );
-    }
-
-    class Controller {
-        String getDate() {
-            final var now = LocalDateTime.now();
-            return now.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
-        }
-    }
-
-    @Test
-    void itShouldReturnARecentTime() {
-        final var controller = new Controller();
-        final var expected = LocalDateTime.of(2020, 1, 1, 1, 1, 1, 1);
-        Mockito.mockStatic(LocalDateTime.class).when(() -> LocalDateTime.now())
-                .thenReturn(expected);
-        assertEquals("01-01-2020", controller.getDate());
     }
 }
