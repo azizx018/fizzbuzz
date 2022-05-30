306f3009c9007f4280858b32321d2524eb5e754f
azizx018 <chiara.aziz@gmail.com>
2022-05-29 20:03:29 -0500

    register endpoint tested

--- user/src/test/java/net/yorksolutions/user/UserApplicationTests.java
+++ user/src/test/java/net/yorksolutions/user/UserApplicationTests.java
@@ -113,4 +113,29 @@ class UserApplicationTests {
 
     }
 
+    @Test
+    void itShouldReturnConflictWhenUsernameTaken() {
+        final TestRestTemplate rest = new TestRestTemplate();
+        final String username = "some username";
+        final String password = "some password";
+        String url = "http://localhost:" + port + "/register?username=" + username + "&password=" + password;
+        when(repository.findByUsername(username))
+                .thenReturn(Optional.of(new UserAccount()));
+        final ResponseEntity<Void> response = rest.getForEntity(url, Void.class);
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+
+    }
+    @Test
+    void itShouldReturnOKWhenUsernameNotTaken() {
+        final TestRestTemplate rest = new TestRestTemplate();
+        final String username = "some username";
+        final String password = "some password";
+        String url = "http://localhost:" + port + "/register?username=" + username + "&password=" + password;
+        when(repository.findByUsername(username))
+                .thenReturn(Optional.empty());
+        final ResponseEntity<Void> response = rest.getForEntity(url, Void.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    }
+
 }
